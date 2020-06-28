fa4dd07de448db6de0657f9158b3b2de26623888
Campbell Hawkes <mr.campbell.hawkes@gmail.com>
2020-06-28 20:34:21 +1200

    Script copies specified directories from target repositories

--- getem.sh
+++ getem.sh
@@ -2,8 +2,10 @@
 Sources=$(jq '.sources' $1)
 SourcesLength=$(echo ${Sources} | jq 'length')
 
+printf "\nRunning getem script \n"
+
 # Setup output dir
-if [ ! -d "$2" ]
+if [ -z ${2+x} ]
 then
 	echo "An output directory must be supplied as the second argument."
 	exit 1
@@ -29,11 +31,11 @@ do
 		if [ -z ${var+x} ]
 			then
 				echo "Source must contain a name, slug, & repo"
-				exit
+				exit 1
 		fi
 	done
 
-	echo "Fetching documentation for $name"
+	printf "\nFetching $name\n\n"
 
 	# Fetch latest from git repo
 	if [ -d "$slug" ]
@@ -44,9 +46,32 @@ do
 	fi
 
 	mkdir ../$2/$slug
+
 	cp $slug/README.md ../$2/$slug/README.md
 
-	echo "Finished getting documentation for $name"
+	if [ ! -z ${categories+x} ]
+	then
+		CategoriesLength=$(echo $categories | jq 'length');
+		echo "Found $CategoriesLength categories"
+		for ((x=0; x < CategoriesLength; x++))
+		do
+			Path=$(echo $categories | jq -r ".[${x}].path")
+			if [ -z ${Path+x} ]
+			then
+				echo "Path for category not provided"
+			else
+				if [ -d "$slug/$Path" ]
+				then
+					cp -r $slug/$Path ../$2/$slug/$Path
+					echo "Copied contents of $Path"
+				else
+					echo "No directory $Path found within $name"
+				fi
+			fi
+		done
+	fi
+
+	printf "\nFinished with $name\n\n"
 done
 
 cd ..
